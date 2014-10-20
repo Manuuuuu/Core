bf8c22ffda2af7b877a458266fe83bca2013e77f
livewa <livewa@gmail.com>
2014-10-21 00:21:00 +0900

    cxcx

--- Common/VPrediction.lua
+++ Common/VPrediction.lua
@@ -1,11 +1,11 @@
 local version = "2.531"
 local TESTVERSION = false
-local AUTOUPDATE = true
-local UPDATE_HOST = "raw.github.com"
-local UPDATE_PATH = "/Hellsing/BoL/master/common/VPrediction.lua".."?rand="..math.random(1,10000)
+local AUTOUPDATE = false
+local UPDATE_HOST = "raw.githubusercontent.com"
+-- local UPDATE_PATH = "/Hellsing/BoL/master/common/VPrediction.lua".."?rand="..math.random(1,10000)
 local UPDATE_FILE_PATH = LIB_PATH.."vPrediction.lua"
-local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
-
+local UPDATE_URL = "https://raw.githubusercontent.com/Hellsing/BoL/master/common/VPrediction.lua"
+--http://botoflegends.com/forum/topic/22778-official-fork-honda7s-repo/
 local function AutoupdaterMsg(msg) print("<font color=\"#6699ff\"><b>VPrediction:</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
 if AUTOUPDATE then
 	local ServerData = GetWebResult(UPDATE_HOST, "/Hellsing/BoL/master/version/VPrediction.version")
